5ec6e4eaeda3b26d93ad03b1b4c04934da9e6ba4
wit <ayakorn@hotmail.com>
2025-07-28 12:38:05 +0700

    change check mail semaphore to mode trap

---
+++ node/lp/zblp_mailsem.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+url=http://localhost:7080/lesspaper
+verbose=false
+export zbhost=__NO__
+export zbserver=__NO__
+export zbhome=~/zabbix
+
+while getopts "f:z:h:s:k:v" opt; do
+    case "$opt" in
+    f)  if [ "$OPTARG" != "__NO__" ]
+        then
+            export url=$OPTARG
+        fi
+        ;;
+    z)  export zbserver=$OPTARG
+        ;;
+    h)  export zbhost=$OPTARG
+        ;;
+    s)  export zbhome=$OPTARG
+        ;;
+    k)  export key=$OPTARG
+        ;;
+    v)  export verbose=true
+        ;;
+    esac
+done
+
+if [ "$url" == "" ]
+then
+    echo "-f http://localhost:7080/lesspaper" >&2
+    echo ZBX_NOTSUPPORTED
+    exit
+fi
+
+url=${url%/}    # remove last /
+
+status=$(curl --connect-timeout 5 -s "$url/status/availableMailSender")
+
+if [ $verbose == true ]
+then
+    echo $status
+else
+    $zbhome/bin/zabbix_sender -z $zbserver -s $zbhost -k "lesspaper.site.$key.mailsemaphore" -o $status
+fi
